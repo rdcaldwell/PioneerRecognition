029645caf5477b9005fd376a19dcb378d9e87e9f
rdcaldwell <rdcald@yahoo.com>
2018-03-31 15:20:34 -0400

    Route for heroku

--- src/app/services/robot.service.ts
+++ src/app/services/robot.service.ts
@@ -8,23 +8,23 @@ export class RobotService {
   constructor(private http: Http) { }
 
   captureImage() {
-    return this.http.get('/api/robot/captureImage').map(res => res.json());
+    return this.http.get('http://10.0.0.223:8080/api/robot/captureImage').map(res => res.json());
   }
 
   turnLeft() {
-    return this.http.get('/api/robot/left').map(res => res.json());
+    return this.http.get('http://10.0.0.223:8080/api/robot/left').map(res => res.json());
   }
 
   turnRight() {
-    return this.http.get('/api/robot/right').map(res => res.json());
+    return this.http.get('http://10.0.0.223:8080/api/robot/right').map(res => res.json());
   }
 
   goForward() {
-    return this.http.get('/api/robot/forward').map(res => res.json());
+    return this.http.get('http://10.0.0.223:8080/api/robot/forward').map(res => res.json());
   }
 
   goBackward() {
-    return this.http.get('/api/robot/backward').map(res => res.json());
+    return this.http.get('http://10.0.0.223:8080/api/robot/backward').map(res => res.json());
   }
 
 }
