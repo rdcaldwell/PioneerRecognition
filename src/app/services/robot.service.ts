9ca2a96f90c5460e7fa04df7b59c57385d4c89ff
rdcaldwell <rdcald@yahoo.com>
2018-04-03 17:37:12 -0400

    Robot server URL

--- src/app/services/robot.service.ts
+++ src/app/services/robot.service.ts
@@ -2,29 +2,32 @@ import { Injectable } from '@angular/core';
 import { Http, RequestOptionsArgs } from '@angular/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
+
 @Injectable()
 export class RobotService {
+
+  private ROBOT_SERVER = 'http://localhost:3333/api/robot';
 
   constructor(private http: Http) { }
 
   captureImage() {
-    return this.http.get('http://10.0.0.223:8080/api/robot/captureImage').map(res => res.json());
+    return this.http.get(`${this.ROBOT_SERVER}/captureImage`).map(res => res.json());
   }
 
   turnLeft() {
-    return this.http.get('http://10.0.0.223:8080/api/robot/left').map(res => res.json());
+    return this.http.get(`${this.ROBOT_SERVER}/left`).map(res => res.json());
   }
 
   turnRight() {
-    return this.http.get('http://10.0.0.223:8080/api/robot/right').map(res => res.json());
+    return this.http.get(`${this.ROBOT_SERVER}/right`).map(res => res.json());
   }
 
   goForward() {
-    return this.http.get('http://10.0.0.223:8080/api/robot/forward').map(res => res.json());
+    return this.http.get(`${this.ROBOT_SERVER}/forward`).map(res => res.json());
   }
 
   goBackward() {
-    return this.http.get('http://10.0.0.223:8080/api/robot/backward').map(res => res.json());
+    return this.http.get(`${this.ROBOT_SERVER}/backward`).map(res => res.json());
   }
 
 }
